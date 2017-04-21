aa91a6aa10c28bee0ab6330f66fb82340c85b601
iramazan <iramazan96@gmail.com>
2017-04-21 12:11:31 -0400

    Fancy Manager/Owner menu. Implementation soon.

--- app/src/main/java/edu/oswego/csc380_2/ManagerActivity.java
+++ app/src/main/java/edu/oswego/csc380_2/ManagerActivity.java
@@ -1,27 +1,38 @@
 package edu.oswego.csc380_2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 import android.view.View;
 
-public class ManagerActivity extends AppCompatActivity implements View.OnClickListener{
+public class ManagerActivity extends Activity {
+
+    private BottomNavigationView bnav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+
+        bnav = (BottomNavigationView) findViewById(R.id.managerBottomNav);
+        bnav.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.managerMenuEdit:
+                        break;
+                    case R.id.managerEmployeeSchedules:
+                        break;
+                    case R.id.managerFinance:
+                        break;
+                    case R.id.managerStock:
+                }
+                return true;
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.menuEdit:
-
-                break;
-            case R.id.employeeSchedule:
-                break;
-            case R.id.stock:
-
-        }
-    }
 }
