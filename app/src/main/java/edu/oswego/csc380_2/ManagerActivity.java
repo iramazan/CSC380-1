8c2343eb9b95ac778171f83b9518842cb4fd5a6d
iramazan <iramazan96@gmail.com>
2017-04-21 19:35:07 -0400

    Manager fragment templates created. Fixed really annoying bug.

--- app/src/main/java/edu/oswego/csc380_2/ManagerActivity.java
+++ app/src/main/java/edu/oswego/csc380_2/ManagerActivity.java
@@ -1,38 +1,50 @@
 package edu.oswego.csc380_2;
 
-import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
 
-public class ManagerActivity extends Activity {
-
-    private BottomNavigationView bnav;
+public class ManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
 
-        bnav = (BottomNavigationView) findViewById(R.id.managerBottomNav);
+        BottomNavigationView bnav = (BottomNavigationView) findViewById(R.id.managerBottomNav);
         bnav.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
                 switch(item.getItemId()) {
                     case R.id.managerMenuEdit:
+                        fragment = ManagerMenuEditFragment.newInstance();
                         break;
                     case R.id.managerEmployeeSchedules:
+                        fragment = ManagerEmployeeScheduleFragment.newInstance();
                         break;
                     case R.id.managerFinance:
+                        fragment = FinanceFragment.newInstance();
                         break;
                     case R.id.managerStock:
+                        fragment = StockFragment.newInstance();
                 }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.managerContainer, fragment);
+                transaction.commit();
                 return true;
             }
         });
+
+        // Display default fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.managerContainer, StockFragment.newInstance());
+        transaction.commit();
     }
 
 }
